86f54d31036010b2a872ca8b6ee7ef4b4d5cf969
adityakmr7 <adityakmr9672@gmail.com>
2023-10-29 13:39:50 +0530

    feat: added stack navigation

--- App.tsx
+++ App.tsx
@@ -2,12 +2,16 @@ import React from "react";
 import AnimationEvent from "./components/animationEvent";
 import BottomSheetContainer from "./components/BottomSheetAnimation/BottomSheetContainer";
 import CircularLoader from "./components/circularLoader";
+import AppStackNavigation from "./navigation/AppStackNavigation";
 // import SharedAnimation from "./components/sharedAnimation";
 export default function App() {
   return (
-    // <SharedAnimation />
-    // <AnimationEvent />
-    // <CircularLoader />
-    <BottomSheetContainer />
+
+      <AppStackNavigation/>
+
   );
 }
+
+// user.name=aditya_fisdom
+// user.email=aditya.kumar@fisdom.com
+
---
+++ components/tabAnimation/TabAnimation.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import {Text, View} from "react-native";
+
+const TabAnimation = () => {
+    return (
+        <View>
+            <Text>Tab Animation</Text>
+        </View>
+    )
+}
+
+export default TabAnimation;
